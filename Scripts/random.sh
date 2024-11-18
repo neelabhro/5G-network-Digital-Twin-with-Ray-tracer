3c028c02a1191593b33872334796d90b9d1f656c
Neelabhro Roy <neelabhro16171@iiitd.ac.in>
2024-11-18 14:18:56 +0100

    Create random.sh

---
+++ Scripts/random.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+cd ../quectel
+source venv/bin/activate
+./venv/bin/python on.py
+ip route add 192.168.70.129/32 via 192.168.225.1
+
+sleep 10
+
+# Ping the server (send only 1 packet) and redirect output to /dev/null
+ping -c 1 192.168.70.129 > /dev/null 2>&1
+
+# Check the exit status of the ping command
+if [ $? -eq 0 ]; then
+    echo "Ping to 192.168.70.129 was successful."
+else
+    echo "Ping to 192.168.70.129 failed."
+fi
